68abb09708723d22242cfd0419e32c3666d388e7
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-04-03 19:23:33 +0530

    back to work

--- timeline.js
+++ timeline.js
@@ -9,7 +9,18 @@ These events will be called 'nodes'.
 These nodes must be added by an admin. (TODO: check session ID, and generate '+' button at the end of the line)
 They will be stored in a database, and will be retrieved to the client-side.
 
+The structure of the node is as such:
+<hexagon><img/></hexagon> <title>
 
+this entire construct is a div, with the class 'node'.
+the hexagon is a div inside it, and the img is a background image of this.
+
+<div class='node'>
+    <div class='hexagon'>
+        <img src='img.png'/>
+    </div>
+    <p class='title'>title</p> 
+</div>
 
 
 
@@ -17,7 +28,11 @@ They will be stored in a database, and will be retrieved to the client-side.
 
 */
 
+//for now it'll add 10 nodes that I hardcoded. later it'll be retrieved from the database.
+
+
 var node = document.createElement("div");
 
 node.className = 'node';
+node.html
 document.body.appendChild(node);
