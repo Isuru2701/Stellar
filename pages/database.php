498d3d46e011dada42f6b2e2f450159adaed06d4
proushoth koushal <proushothkoushal@proushoths-MacBook-Air.local>
2023-04-17 10:46:07 +0530

    testrun

--- pages/database.php
+++ pages/database.php
@@ -10,4 +10,24 @@ if (!$conn)
 {
     die("Connection failed: ". mysqli_connect_error());
 }
+
+   
+                                            //Establishing the forum connection
+    $conn = mysqli_connect("localhost","root","","Blog_Forum");
+
+    $sql= "SELECT * FROM data";
+    $query = mysqli_query($conn,$sql);
+
+   if(isset($_REQUEST["new_post"])){
+       $title = $_REQUEST["title"];
+       $content =$_REQUEST["content"];
+
+       $sql = "Insert INTO data(title, content) values ('$title','$content')";
+       mysqli_query($conn,$sql); 
+
+       header("location: index.php?info=added");
+       exit();
+   }
+
 ?>
+  
