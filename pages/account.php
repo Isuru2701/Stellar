e14e76fa0737db8a3300a213beff0e14ad2b6a2d
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-04-30 07:48:44 +0530

    added icon tag to account

--- pages/account.php
+++ pages/account.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Account</title>
+    <link rel="icon" href="../resources/favicon.ico" type="image/ico">
     <link rel="stylesheet" href="../styles/create.css">
     <link rel="stylesheet" href="../styles/account.css">
 </head>
